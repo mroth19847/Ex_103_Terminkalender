05c58c0ef6bbc148575fe045d2a9fef5cf7b0a50
mroth19847 <rotmac16@htl-kaindorf.at>
2018-09-28 13:12:13 +0200

    added AppointmentModel class

---
+++ Bsp_103_TerminKalender/src/AppointmentModel.java
@@ -0,0 +1,32 @@
+
+import java.util.ArrayList;
+import javax.swing.AbstractListModel;
+
+
+public class AppointmentModel extends AbstractListModel{
+
+    private ArrayList<Appointment> app = new ArrayList<>();
+    @Override
+    public int getSize() {
+        return app.size();
+    }
+
+    @Override
+    public Object getElementAt(int index) {
+        return app.get(index);
+    }
+    
+    public void add(Appointment a){
+        app.add(a);
+        fireIntervalAdded(this, app.size()-1, app.size()-1);
+    }
+    
+    public void load(){
+        
+    }
+    
+    public void save(){
+        
+    }
+    
+}
